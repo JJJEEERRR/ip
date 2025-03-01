4c865092331463c8a74c5080ee6b5389e1b18a2b
JJJEEERRR <player63696@gmail.com>
2025-03-01 23:12:39 +0800

    Level-1: Achiving Echo amd Bye

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
-        String logo = " /\\_/\\  \n" +
-                      "( o.o ) \n" +
-                      " > ^ <  \n";
+        String logo = """
+                 /\\_/\\ \s
+                ( o.o )\s
+                 > ^ < \s
+                """;
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Buddy");
         System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
