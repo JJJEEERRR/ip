78cd557a2f521f2e403f54e0559a16a55670adc0
JJJEEERRR <player63696@gmail.com>
2025-03-01 23:36:02 +0800

    Level-2: Add, List

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,7 +1,15 @@
-import java.util.Scanner;
+import java.util.Scanner; //保留Scanner的import
 
 public class Buddy {
+    private static final int MAX_TASKS = 100;
+    private String[] tasks = new String[MAX_TASKS];
+    private int taskCount = 0;
+
     public static void main(String[] args) {
+        new Buddy().run();
+    }
+
+    public void run() {
         String logo = """
                  /\\_/\\ \s
                 ( o.o )\s
@@ -18,11 +26,34 @@ public class Buddy {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                listTasks();
+            } else {
+                addTask(input);
             }
-            System.out.println(input);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
+    }
+
+    private void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
+            System.out.println("  ____________________________________________________________");
+            System.out.println("    added: " + task);
+            System.out.println("  ____________________________________________________________");
+        } else {
+            System.out.println("Task list is full.");
+        }
+    }
+
+    private void listTasks() {
+        System.out.println("  ____________________________________________________________");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("  " + (i + 1) + ". " + tasks[i]);
+        }
+        System.out.println("  ____________________________________________________________");
     }
 }
