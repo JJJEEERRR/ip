799d41f73b4e529755d4ff895bce9eda462cea87
JJJEEERRR <player63696@gmail.com>
2025-03-01 22:45:23 +0800

    Level-0: Rename Duke.java to Buddy.java and update .gitignore

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,11 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String logo = " /\\_/\\  \n" +
+                      "( o.o ) \n" +
+                      " > ^ <  \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Buddy");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
